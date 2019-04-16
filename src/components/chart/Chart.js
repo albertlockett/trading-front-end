737494184ab3d7cd8d74feb86b76e85cd38dc3b5
Albert Lockett <albert.lockett@sonraisecurity.com>
2019-04-16 07:17:03 -0300

    added volume and high and low

--- src/components/chart/Chart.js
+++ src/components/chart/Chart.js
@@ -7,21 +7,6 @@ class Chart extends Component {
 
   constructor(props, context) {
     super(props, context)
-
-    // this.data = [
-    //   { open: 1, close: 2 },
-    //   { open: 2, close: 5 },
-    //   { open: 5, close: 3 },
-    //   { open: 2, close: 1 },
-    //   { open: 4, close: 2 },
-    //   { open: 2, close: 5 },
-    //   { open: 5, close: 10 },
-    //   { open: 3, close: 1 },
-    //   { open: 1, close: 2 },
-    //   { open: 18, close: 20 },
-    //   { open: 19, close: 3 },
-    //   { open: 3, close: 1 },
-    // ]
     this.data = data;
   }
 
@@ -29,20 +14,37 @@ class Chart extends Component {
     this.drawChart();
   }
 
+  /**
+   * Color green or red depending on price move for period
+   */
+  color(d, i) {
+    if (d.open < d.close) {
+      return "green"
+    } else {
+      return "red"
+    }
+  }
+
   drawChart() {
     console.log("IT RAN!");
     
     const height = 300;
     const width = 1500;
+    const volumePart = 0.2;
 
     const maxPrice = d3.max(this.data, d => Math.max(d.open, d.close))
     const minPrice = d3.min(this.data, d => Math.min(d.open, d.close))
+    const maxVolume = d3.max(this.data, d => d.volume)
 
     console.log({ minPrice, maxPrice, diff: maxPrice - minPrice})
 
     const yScale = d3.scaleLinear()
       .domain([minPrice, maxPrice])
-      .range([height, 0]);
+      .range([height - height * volumePart, 0]);
+
+    const volumeScale = d3.scaleLinear()
+      .domain([0, maxVolume])
+      .range([height * volumePart, 0])
 
     const barContainerWidth = width / this.data.length;
     
@@ -52,21 +54,36 @@ class Chart extends Component {
     .attr("height", height)
     .attr("class", "chart-container");
                   
-    svg.selectAll("rect")
+    svg.selectAll("rect.price")
       .data(this.data)
       .enter()
       .append("rect")
-      .attr("x", (d, i) => i * barContainerWidth)
-      .attr("y", d => yScale(Math.max(d.open, d.close)))
-      .attr("width", barContainerWidth - 2)
-      .attr("height", (d, i) => Math.abs(yScale(d.open) - yScale(d.close)))
-      .attr("fill", (d, i) => {
-        if (d.open < d.close) {
-          return "green"
-        } else {
-          return "red"
-        }
-      })
+        .attr("x", (d, i) => i * barContainerWidth)
+        .attr("y", d => yScale(Math.max(d.open, d.close)))
+        .attr("width", barContainerWidth - 2)
+        .attr("height", (d, i) => Math.abs(yScale(d.open) - yScale(d.close)))
+        .attr("fill", (d, i) => this.color(d, i))
+    
+    svg.selectAll("rect.volume")
+      .data(this.data)
+      .enter()    
+      .append("rect")
+        .attr("x", (d, i) => i * barContainerWidth)
+        .attr("y", d => height - height * volumePart + volumeScale(d.volume))
+        .attr("width", barContainerWidth - 2)
+        .attr("height", (d, i) => volumeScale(0) - volumeScale(d.volume))
+        .attr("fill", (d, i) => this.color(d, i))
+    
+    svg.selectAll("line")
+      .data(this.data)
+      .enter()
+      .append("line")
+      .attr("x1", (d, i) => i * barContainerWidth + barContainerWidth / 2 - 1)
+      .attr("x2", (d, i) => i * barContainerWidth + barContainerWidth / 2 - 1)
+      .attr("y1", d => yScale(d.high))
+      .attr("y2", d => yScale(d.low))
+      .attr("stroke", (d, i) => this.color(d, i))
+      .attr("strokeWidth", 5)
   }
 
   render() {
